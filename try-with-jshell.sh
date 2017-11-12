67266d9fe66082b1c55e68bb1a84a5922a6ba31c
Hakan Ozler <ozler.hakan@gmail.com>
2017-11-12 23:08:49 +0300

    use one tr operation [skip ci]

--- try-with-jshell.sh
+++ try-with-jshell.sh
@@ -6,6 +6,6 @@ rm -rf jshell/dependencies/
 
 mvn dependency:copy-dependencies -DoutputDirectory=jshell/dependencies/ >/dev/null 2>&1
 
-DEPS=$(ls -d -1 jshell/dependencies/** | tr '\n' ' ' |  tr ' ' ':')
+DEPS=$(ls -d -1 jshell/dependencies/** | tr '\n' ':')
 
 jshell --class-path ${DEPS}target/poiji.jar jshell/snippets
