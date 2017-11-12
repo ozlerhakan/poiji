f3f6b49d5b4bf867fa52bbf82643970f433934c6
Hakan Ozler <ozler.hakan@gmail.com>
2017-11-12 22:57:00 +0300

    try with jshell [skip ci]

---
+++ try-with-jshell.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+
+rm -rf jshell/dependencies/
+
+mvn dependency:copy-dependencies -DoutputDirectory=jshell/dependencies/ >/dev/null 2>&1
+
+DEPS=$(ls -d -1 jshell/dependencies/** | tr '\n' ' ' |  tr ' ' ':')
+
+jshell --class-path ${DEPS}target/poiji.jar jshell/snippets
