e4888aa5c2317746b1875804ad4d150b7b52adc1
Hakan Ozler <ozler.hakan@gmail.com>
2018-03-12 12:43:59 +0300

    update test case

--- src/test/java/com/poiji/deserialize/DerializersEmptyCellTest.java
+++ src/test/java/com/poiji/deserialize/DerializersEmptyCellTest.java
@@ -1,13 +1,17 @@
 package com.poiji.deserialize;
 
-import com.poiji.deserialize.model.Employee;
 import com.poiji.bind.Poiji;
+import com.poiji.deserialize.model.Employee;
+import com.poiji.exception.PoijiExcelType;
 import com.poiji.option.PoijiOptions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,6 +21,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 /**
  * Created by hakan on 17/01/2017.
@@ -26,37 +31,51 @@ public class DerializersEmptyCellTest {
 
     private String path;
     private List<Employee> expectedEmployess;
+    private boolean fromStream;
 
-    public DerializersEmptyCellTest(String path, List<Employee> expectedEmployess) {
+    public DerializersEmptyCellTest(String path, List<Employee> expectedEmployess, boolean fromStream) {
         this.path = path;
         this.expectedEmployess = expectedEmployess;
+        this.fromStream = fromStream;
     }
 
     @Parameterized.Parameters(name = "{index}: ({0})={1}")
-    public static Iterable<Object[]> queries() throws Exception {
+    public static Iterable<Object[]> queries() {
         return Arrays.asList(new Object[][]{
-                {"src/test/resources/employeeswithemptycells.xlsx", unmarshalling()},
+                {"src/test/resources/employeeswithemptycells.xlsx", unmarshalling(), false},
+                {"src/test/resources/employeeswithemptycells.xlsx", unmarshalling(), true},
         });
     }
 
     @Test
     public void shouldMapEmptyCellsAndSkipTwoRows() {
 
-            PoijiOptions options = PoijiOptionsBuilder.settings(2).build();
-            List<Employee> actualEmployees = Poiji.fromExcel(new File(path), Employee.class, options);
+        PoijiOptions options = PoijiOptionsBuilder.settings(2).build();
+        List<Employee> actualEmployees;
 
-            assertThat(actualEmployees, notNullValue());
-            assertThat(actualEmployees.size(), not(0));
-            assertThat(actualEmployees.size(), is(expectedEmployess.size()));
+        if (fromStream) {
+            try (InputStream stream = new FileInputStream(new File(path))) {
+                actualEmployees = Poiji.fromExcel(stream, PoijiExcelType.XLSX, Employee.class, options);
+            } catch (IOException e) {
+                fail(e.getMessage());
+                return;
+            }
+        } else {
+            actualEmployees = Poiji.fromExcel(new File(path), Employee.class, options);
+        }
 
-            Employee actualEmployee1 = actualEmployees.get(0);
-            Employee actualEmployee2 = actualEmployees.get(1);
+        assertThat(actualEmployees, notNullValue());
+        assertThat(actualEmployees.size(), not(0));
+        assertThat(actualEmployees.size(), is(expectedEmployess.size()));
 
-            Employee expectedEmployee1 = expectedEmployess.get(0);
-            Employee expectedEmployee2 = expectedEmployess.get(1);
+        Employee actualEmployee1 = actualEmployees.get(0);
+        Employee actualEmployee2 = actualEmployees.get(1);
 
-            assertThat(actualEmployee1.toString(), is(expectedEmployee1.toString()));
-            assertThat(actualEmployee2.toString(), is(expectedEmployee2.toString()));
+        Employee expectedEmployee1 = expectedEmployess.get(0);
+        Employee expectedEmployee2 = expectedEmployess.get(1);
+
+        assertThat(actualEmployee1.toString(), is(expectedEmployee1.toString()));
+        assertThat(actualEmployee2.toString(), is(expectedEmployee2.toString()));
 
     }
 
