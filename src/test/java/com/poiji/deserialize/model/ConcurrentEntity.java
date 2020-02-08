6fe730885879b44278874713c0d3d747f2692c94
vaa25 <alex.vlasov.ua@gmail.com>
2020-02-09 01:53:55 +0200

    #127 saving: remove lombok

--- src/test/java/com/poiji/deserialize/model/ConcurrentEntity.java
+++ src/test/java/com/poiji/deserialize/model/ConcurrentEntity.java
@@ -3,11 +3,9 @@ package com.poiji.deserialize.model;
 import com.poiji.annotation.ExcelCell;
 import com.poiji.annotation.ExcelCellName;
 import com.poiji.annotation.ExcelSheet;
-import lombok.Data;
-import lombok.experimental.Accessors;
+import java.util.Objects;
+import java.util.StringJoiner;
 
-@Data
-@Accessors(chain = true)
 @ExcelSheet("test")
 public final class ConcurrentEntity {
 
@@ -16,4 +14,46 @@ public final class ConcurrentEntity {
     @ExcelCellName(value = "TexT")
     private String text;
 
+    public long getPrimitiveLong() {
+        return primitiveLong;
+    }
+
+    public ConcurrentEntity setPrimitiveLong(final long primitiveLong) {
+        this.primitiveLong = primitiveLong;
+        return this;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public ConcurrentEntity setText(final String text) {
+        this.text = text;
+        return this;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final ConcurrentEntity that = (ConcurrentEntity) o;
+        return primitiveLong == that.primitiveLong && Objects.equals(text, that.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(primitiveLong, text);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", ConcurrentEntity.class.getSimpleName() + "[", "]")
+            .add("primitiveLong=" + primitiveLong)
+            .add("text='" + text + "'")
+            .toString();
+    }
 }
