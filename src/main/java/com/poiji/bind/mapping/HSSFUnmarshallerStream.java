11f44d890fd01718db0e3c5a032c6eb3d9231113
Igor Volkov <virtual-machinist@users.noreply.github.com>
2024-03-24 18:46:43 +0200

    Improve blank mandatory cell handling for XLS files and Sheet instances

--- src/main/java/com/poiji/bind/mapping/HSSFUnmarshallerStream.java
+++ src/main/java/com/poiji/bind/mapping/HSSFUnmarshallerStream.java
@@ -3,6 +3,7 @@ package com.poiji.bind.mapping;
 import com.poiji.bind.PoijiInputStream;
 import com.poiji.exception.PoijiException;
 import com.poiji.option.PoijiOptions;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
@@ -23,12 +24,14 @@ final class HSSFUnmarshallerStream extends HSSFUnmarshaller {
     @Override
     protected Workbook workbook() {
         try {
-
+            Workbook workbook;
             if (options.getPassword() != null) {
-                return WorkbookFactory.create(poijiInputStream.stream(), options.getPassword());
+                workbook = WorkbookFactory.create(poijiInputStream.stream(), options.getPassword());
+            } else {
+                workbook = WorkbookFactory.create(poijiInputStream.stream());
             }
-
-            return WorkbookFactory.create(poijiInputStream.stream());
+            workbook.setMissingCellPolicy(Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
+            return workbook;
         } catch (IOException e) {
             throw new PoijiException("Problem occurred while creating HSSFWorkbook", e);
         }
--- src/test/java/com/poiji/deserialize/MandatoryCellsExceptionTest.java
+++ src/test/java/com/poiji/deserialize/MandatoryCellsExceptionTest.java
@@ -2,18 +2,20 @@ package com.poiji.deserialize;
 
 import com.poiji.bind.Poiji;
 import com.poiji.deserialize.model.byname.MandatoryMissingCells;
+import com.poiji.exception.PoijiExcelType;
 import com.poiji.exception.PoijiMultiRowException;
 import com.poiji.exception.PoijiMultiRowException.PoijiRowSpecificException;
 import com.poiji.option.PoijiOptions;
-
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -22,11 +24,63 @@ import static org.junit.Assert.fail;
 public class MandatoryCellsExceptionTest {
 
     @Test
-    public void testExcelMandatoryColumn() {
+    public void shouldThrowExceptionForMissingCell() {
         try {
             Poiji.fromExcel(createDummyExcel(), MandatoryMissingCells.class, PoijiOptions.PoijiOptionsBuilder
                     .settings()
                     .build());
+        } catch (PoijiMultiRowException e) {
+            List<PoijiRowSpecificException> errors = e.getErrors();
+            assertEquals(1, errors.size());
+            assertEquals("Address", errors.get(0).getColumnName());
+            assertEquals("address", errors.get(0).getFieldName());
+            assertEquals((Integer) 1, errors.get(0).getRowNum());
+            return;
+        }
+        fail("Expected exception: " + PoijiMultiRowException.class.getName());
+    }
+
+    @Test
+    public void shouldThrowExceptionForBlankCellInSheet() throws IOException {
+        try (InputStream stream = new FileInputStream("src/test/resources/blank-cell.xlsx");
+             XSSFWorkbook workbook = new XSSFWorkbook(stream)) {
+            workbook.setMissingCellPolicy(Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            Poiji.fromExcel(sheet, MandatoryMissingCells.class, PoijiOptions.PoijiOptionsBuilder
+                    .settings()
+                    .build());
+        } catch (PoijiMultiRowException e) {
+            List<PoijiRowSpecificException> errors = e.getErrors();
+            assertEquals(1, errors.size());
+            assertEquals("Address", errors.get(0).getColumnName());
+            assertEquals("address", errors.get(0).getFieldName());
+            assertEquals((Integer) 1, errors.get(0).getRowNum());
+            return;
+        }
+        fail("Expected exception: " + PoijiMultiRowException.class.getName());
+    }
+
+    @Test
+    public void shouldThrowExceptionForBlankCellInFile() {
+        try {
+            Poiji.fromExcel(new File("src/test/resources/blank-cell.xls"), MandatoryMissingCells.class,
+                    PoijiOptions.PoijiOptionsBuilder.settings().build());
+        } catch (PoijiMultiRowException e) {
+            List<PoijiRowSpecificException> errors = e.getErrors();
+            assertEquals(1, errors.size());
+            assertEquals("Address", errors.get(0).getColumnName());
+            assertEquals("address", errors.get(0).getFieldName());
+            assertEquals((Integer) 1, errors.get(0).getRowNum());
+            return;
+        }
+        fail("Expected exception: " + PoijiMultiRowException.class.getName());
+    }
+
+    @Test
+    public void shouldThrowExceptionForBlankCellInStream() throws IOException {
+        try (InputStream stream = new FileInputStream("src/test/resources/blank-cell.xls")) {
+            Poiji.fromExcel(stream, PoijiExcelType.XLS, MandatoryMissingCells.class,
+                    PoijiOptions.PoijiOptionsBuilder.settings().build());
         } catch (PoijiMultiRowException e) {
             List<PoijiRowSpecificException> errors = e.getErrors();
             assertEquals(1, errors.size());
